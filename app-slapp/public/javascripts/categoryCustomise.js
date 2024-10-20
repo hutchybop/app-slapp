adb138331b76ac58378b90c8d083d1bcd9a6c16d
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2024-10-20 19:57:18 +0100

    app-slapp-20241020-1957

---
+++ app-slapp/public/javascripts/categoryCustomise.js
@@ -0,0 +1,32 @@
+Sortable.create(orderCategories, {
+    group: "sorting",
+    animation: 150,
+    ghostClass: 'blue-background-class',
+    sort: true
+});
+
+
+
+let info = document.querySelector('#info')
+
+info.addEventListener('submit', () => {
+
+    const cat = document.querySelectorAll('.cat')
+
+    for(let i = 0; i < cat.length; i++){
+        let obj = {}
+        obj[cat[i].name] = cat[i].value
+        field = document.createElement('input');
+        field.type = 'hidden';
+        field.name = cat[i].value;
+        info.appendChild(field);
+        field.value = cat[i].name;
+    }
+
+})
+
+
+
+
+
+
---
+++ app-slapp/utils/newUserSeed.js
@@ -0,0 +1,85 @@
+const { Category } = require("../models/category")
+const { Ingredient } = require('../models/ingredient')
+const { Meal } = require('../models/meal')
+
+module.exports.newUserSeed = async (userId) => {
+
+    const newCategory = new Category({
+        catList: [
+            'Toiletries',
+            'Veg 1',
+            'Veg 2',
+            'Veg 3',
+            'Veg 4',
+            'Meat And dairy',
+            'Beans And Spices',
+            'Middle store',
+            'Milk And bread',
+            'Frozen',
+            'Non food',
+        ],
+        author: userId
+    })
+    await newCategory.save();
+    
+    // 'Defualt' user ID to be seeded from
+     const defaultUserId = process.env.DEFAULT_MEALS_ID
+    
+
+    // Searches for all the defaultUser ingredients
+    const defaultUserIngredients = await Ingredient.find({author: defaultUserId})
+    // Loops each ingredient and adds it to the user's ingredients
+    for(i in defaultUserIngredients){
+        await Ingredient.create({ name: defaultUserIngredients[i].name, cat: defaultUserIngredients[i].cat, author: userId })
+    }
+
+    // // Searches for all the defaultUser meals
+    const defaultUserMeals = await Meal.find({author: defaultUserId})        
+        .populate({ path: 'weeklyItems', populate: { path: 'weeklyIngredients' } })
+        .populate({ path: 'replaceOnUse', populate: { path: 'replaceOnUseIngredients' } })
+
+    // Loops all the meals
+    for(m in defaultUserMeals){
+
+        // Need to create unique weekly items
+        let weeklyItems = []
+        for(i in defaultUserMeals[m].weeklyItems){
+            let name = defaultUserMeals[m].weeklyItems[i].weeklyIngredients.name
+            let qty = defaultUserMeals[m].weeklyItems[i].qty
+            let weeklyIng = await Ingredient.findOne({
+                $and: [{ name }, { author: userId }]
+            })
+            weeklyItems.push({
+                qty, 
+                weeklyIngredients: weeklyIng
+            })
+        }
+
+        // Need to create unique replaceOnUse items
+        let replaceOnUseItems = []
+        for(i in defaultUserMeals[m].replaceOnUse){
+            let name = defaultUserMeals[m].replaceOnUse[i].replaceOnUseIngredients.name
+            let qty = defaultUserMeals[m].replaceOnUse[i].qty
+            let replaceOnUseIng = await Ingredient.findOne({
+                $and: [{ name }, { author:userId }]
+            })
+            replaceOnUseItems.push({
+                qty, 
+                replaceOnUseIngredients: replaceOnUseIng
+            })
+        }
+
+        // Creating the new meal
+        const newMeal = new Meal({
+            default: defaultUserMeals[m].default,
+            mealName: defaultUserMeals[m].mealName,
+            mealRecipe: defaultUserMeals[m].mealRecipe,
+            weeklyItems: weeklyItems,
+            replaceOnUse: replaceOnUseItems,
+            mealType: defaultUserMeals[m].mealType,
+            author: userId
+        })
+        await newMeal.save();
+
+    }
+}
