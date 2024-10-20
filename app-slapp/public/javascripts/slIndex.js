0dc6fa1e571507424c8318e6ef91d199184f8e7b
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2024-10-20 19:59:18 +0100

    app-slapp-20241020-1959

--- app-slapp/public/javascripts/slIndex.js
+++
@@ -1,7 +0,0 @@
-let slIndexAlertDelete = document.getElementById('slIndexAlertDelete')
-slIndexAlertDelete.onsubmit = () => {
-    if(!confirm("Do you really want to delete this Shopping List?")) {
-      return false;
-    }
-    this.form.submit();
-}
