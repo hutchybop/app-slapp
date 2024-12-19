6315d22cd661fb1eba46e3341fb139a5b0daa359
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2024-12-19 21:49:25 +0000

    app-slapp-20241219-2149

--- restart/initialEnCrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SALT=$(echo "Y29tcGxleF9zYWx0X3ZhbHVlXzEyMzQ=" | base64 -d)
-USER=$(whoami)
-HOSTNAME=$(hostname)
-DECRYPTION_KEY=$(echo -n "$SALT$USER$HOSTNAME" | sha256sum | head -c 32)
-
-# Encrypt the .config file using AES-256-CBC with PBKDF2
-openssl enc -aes-256-cbc -salt -pbkdf2 -in .configSlapp -out .configSlapp.enc -pass pass:$DECRYPTION_KEY
