0aeb431307cf4d27cf90aa000c6ae748d52518ff
Chris Hutchinson <64486595+hutchybop@users.noreply.github.com>
2025-04-12 00:13:30 +0100

    app-slapp-20250412-0013

--- app-slapp/public/javascripts/ingredientEdit.js
+++
@@ -1,7 +0,0 @@
-let ingEditAlertDelete = document.getElementById('ingEditAlertDelete')
-ingEditAlertDelete.onsubmit = () => {
-    if(!confirm("Do you really want to delete this Ingredient?")) {
-      return false;
-    }
-    this.form.submit();
-}
--- app-slapp/public/javascripts/mealsShow.js
+++
@@ -1,7 +0,0 @@
-let mealsShowAlertDelete = document.getElementById('mealsShowDelete')
-mealsShowAlertDelete.onsubmit = () => {
-    if(!confirm("Do you really want to delete this Meal?")) {
-      return false;
-    }
-    this.form.submit();
-}
